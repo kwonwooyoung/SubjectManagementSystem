d56cc3d238eb05e906f556ffffc58a08a1e6c830
kwonwooyoung <dndud710@nate.com>
2022-03-27 15:53:54 +0900

    add manu function

--- src/SubjectManagementSystem.java
+++ src/SubjectManagementSystem.java
@@ -15,44 +15,53 @@ public class SubjectManagementSystem {
 		System.out.print("Select one number 1-6:");
 		x = input.nextInt();
 		if (x == 1) {
-			System.out.print("Your ID: ");
-			int id = input.nextInt();
-			System.out.print("Add Subject Name: ");
-			String subjectName = input.next();
-			System.out.print("Subject professor Name: ");
-			String professorName = input.next();
+			add();
 		}
 		else if (x == 2) {
-			System.out.print("Your ID: ");
-			int id = input.nextInt();
-			System.out.print("Delete Subject Name: ");
-			String subjectName = input.next();
-			System.out.print("Subject professor Name: ");
-			String professorName = input.next();
+			delete();
 		}
 		else if (x == 3) {
-			System.out.print("Your ID: ");
-			int id = input.nextInt();
-			System.out.print("Edit Subject Name: ");
-			String subjectName = input.next();
-			System.out.print("Edit Subject Professor Name: ");
-			String professorName = input.next();
+			edit();
 		}
 		else if (x == 4) {
-			System.out.print("Your ID: ");
-			int id = input.nextInt();
+			view();
+			
 		}
 		else if (x == 5) {
-			System.out.println("*** Subject Management System Menu ***");
-			System.out.println("1. Add Subjects");
-			System.out.println("2. Delete Subjects");
-			System.out.println("3. Edit Subjects");
-			System.out.println("4. View Subjects");
-			System.out.println("5. Show a menu");
-			System.out.println("6. Exit");
-			System.out.println("Select one number 1-6:");
-			x = input.nextInt();
 		}
 		}
 	}
+	public static void add() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Your ID: ");
+		int id = input.nextInt();
+		System.out.print("Add Subject Name: ");
+		String subjectName = input.next();
+		System.out.print("Add Subject professor Name: ");
+		String professorName = input.next();
+	}
+	public static void delete() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Your ID: ");
+		int id = input.nextInt();
+		System.out.print("Delete Subject Name: ");
+		String subjectName = input.next();
+		System.out.print("Delete Subject professor Name: ");
+		String professorName = input.next();
+	}
+	public static void edit() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Your ID: ");
+		int id = input.nextInt();
+		System.out.print("Edit Subject Name: ");
+		String subjectName = input.next();
+		System.out.print("Edit Subject Professor Name: ");
+		String professorName = input.next();
+	}
+	public static void view() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Your ID: ");
+		int id = input.nextInt();
+	}
 }
+
