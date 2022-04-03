5d6b9a177eb266e3a873354278d2834b23f3dea2
kwonwooyoung <dndud710@nate.com>
2022-04-03 23:48:01 +0900

    add

--- src/SubjectManagementSystem.java
+++ src/SubjectManagementSystem.java
@@ -2,66 +2,35 @@ import java.util.Scanner;
 
 public class SubjectManagementSystem {
 	public static void main (String[] args) {
-		int x = 0;
 		Scanner input = new Scanner(System.in);
-		while (x != 6) {
+		Subject subject = new Subject(input);
+		int x = 0;
+		while (x != 5) {
 		System.out.println("*** Subject Management System Menu ***");
 		System.out.println("1. Add Subjects");
 		System.out.println("2. Delete Subjects");
 		System.out.println("3. Edit Subjects");
 		System.out.println("4. View Subjects");
-		System.out.println("5. Show a menu");
-		System.out.println("6. Exit");
-		System.out.print("Select one number 1-6:");
+		System.out.println("5. Exit");
+		System.out.print("Select one number 1-5:");
 		x = input.nextInt();
 		if (x == 1) {
-			add();
+			subject.add();
 		}
 		else if (x == 2) {
-			delete();
+			subject.delete();
 		}
 		else if (x == 3) {
-			edit();
+			subject.edit();
 		}
 		else if (x == 4) {
-			view();
-			
+			subject.view();
 		}
-		else if (x == 5) {
+		else {
+			continue;
 		}
 		}
 	}
-	public static void add() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Your ID: ");
-		int id = input.nextInt();
-		System.out.print("Add Subject Name: ");
-		String subjectName = input.next();
-		System.out.print("Add Subject professor Name: ");
-		String professorName = input.next();
-	}
-	public static void delete() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Your ID: ");
-		int id = input.nextInt();
-		System.out.print("Delete Subject Name: ");
-		String subjectName = input.next();
-		System.out.print("Delete Subject professor Name: ");
-		String professorName = input.next();
-	}
-	public static void edit() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Your ID: ");
-		int id = input.nextInt();
-		System.out.print("Edit Subject Name: ");
-		String subjectName = input.next();
-		System.out.print("Edit Subject Professor Name: ");
-		String professorName = input.next();
-	}
-	public static void view() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("Your ID: ");
-		int id = input.nextInt();
-	}
+
 }
 
