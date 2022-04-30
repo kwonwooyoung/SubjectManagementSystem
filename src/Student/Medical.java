8c0d901f6dbcf84746fcbe5e882778c8bfdd631e
kwonwooyoung <dndud710@nate.com>
2022-04-30 15:02:59 +0900

    good

---
+++ src/Student/Medical.java
@@ -0,0 +1,22 @@
+package Student;
+
+import java.util.Scanner;
+
+public class Medical extends Student {
+	public Medical(StudentKind kind) {
+		super(kind);
+	}
+	public void getUserInput(Scanner input) {
+		System.out.print("Your ID: ");
+		int id = input.nextInt();
+		this.setId(id);
+		
+		System.out.print("Add Subject Name: ");
+		String subjectName = input.next();
+		this.setSubjectName(subjectName);
+		
+		System.out.print("Add Subject professor Name: ");
+		String professorName = input.next();
+		this.setProfessorName(professorName);
+	}
+}
---
+++ src/Student/Operation.java
@@ -0,0 +1,25 @@
+package Student;
+
+import java.util.Scanner;
+
+public class Operation extends Student {
+	public Operation(StudentKind kind) {
+		super(kind);
+	}
+	public void getUserInput(Scanner input) {
+		
+		
+		System.out.print("Your ID: ");
+		int id = input.nextInt();
+		this.setId(id);
+		
+		System.out.print("Add Subject Name: ");
+		String subjectName = input.next();
+		this.setSubjectName(subjectName);
+		
+		System.out.print("Add Subject professor Name: ");
+		String professorName = input.next();
+		this.setProfessorName(professorName);
+	}
+
+}
