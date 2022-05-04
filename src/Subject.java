3469534c4e66ba90311864483adb85dd2f66a8c4
kwonwooyoung <dndud710@nate.com>
2022-05-04 14:30:01 +0900

    yes

--- src/Subject.java
+++ src/Subject.java
@@ -1,14 +1,16 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import Student.EngineeringStudent;
 import Student.Liberalarts;
 import Student.Medical;
 import Student.Operation;
 import Student.Student;
+import Student.StudentInput;
 import Student.StudentKind;
 
 public class Subject {
-	ArrayList<Student> students = new ArrayList<Student>();
+	ArrayList<StudentInput> students = new ArrayList<StudentInput>();
 	Scanner input;
 	int a;
 	Subject(Scanner input){
@@ -16,7 +18,7 @@ public class Subject {
 	}
 	public void add() {
 		int kind = 0;
-		Student student;
+		StudentInput studentInput;
 		while (kind != 1 && kind != 2 && kind != 1 && kind != 2) {
 			System.out.println(" 1 for engineering");
 			System.out.println(" 2 for Operation ");
@@ -25,27 +27,27 @@ public class Subject {
 			System.out.print("Select num 1~4 for Student Kind: ");
 			kind = input.nextInt();
 			if(kind == 1) {
-				student = new Student(StudentKind.Engineering);
-				student.getUserInput(input); 
-				students.add(student);
+				studentInput = new EngineeringStudent(StudentKind.Engineering);
+				studentInput.getUserInput(input); 
+				students.add(studentInput);
 				break;
 			}
 			else if(kind == 2) {
-				student = new Operation(StudentKind.Operation);
-				student.getUserInput(input);
-				students.add(student);
+				studentInput = new Operation(StudentKind.Operation);
+				studentInput.getUserInput(input);
+				students.add(studentInput);
 				break;
 			}
 			else if(kind == 3) {
-				student = new Medical(StudentKind.Medical);
-				student.getUserInput(input);
-				students.add(student);
+				studentInput = new Medical(StudentKind.Medical);
+				studentInput.getUserInput(input);
+				students.add(studentInput);
 				break;
 			}
 			else if(kind == 4) {
-				student = new Liberalarts(StudentKind.Liberalarts);
-				student.getUserInput(input);
-				students.add(student);
+				studentInput = new Liberalarts(StudentKind.Liberalarts);
+				studentInput.getUserInput(input);
+				students.add(studentInput);
 				break;
 			}
 			else {
@@ -81,7 +83,7 @@ public class Subject {
 		System.out.print("Your ID: ");
 		int id = input.nextInt();
 		for(int i = 0; i <students.size(); i++) {
-			Student student = students.get(i);
+			StudentInput studentInput = students.get(i);
 			if (students.get(i).getId() == id) {
 				int num = -1;
 				while(num != 2) {
@@ -93,12 +95,12 @@ public class Subject {
 					if(num==1) {
 						System.out.print("편집할 과목을 고르시오 : ");
 						String subjectName = input.next();
-						student.setSubjectName(subjectName);
+						studentInput.setSubjectName(subjectName);
 					}
 					if(num==2) {
 						System.out.print("편집할 과목의 교수님 성함을 입력하시오 : ");
 						String professorName = input.next();
-						student.setProfessorName(professorName);
+						studentInput.setProfessorName(professorName);
 					}
 					else {
 						continue;
